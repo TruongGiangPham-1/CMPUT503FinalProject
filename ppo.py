c6f9865d13378d6eccb2fac2cda173b8d53da39a
TruongGiangPham-1 <truonggi@ualberta.ca>
2025-04-12 15:07:56 -0600

    Fix: eval forgot to convert obs to torch

--- ppo.py
+++ ppo.py
@@ -141,6 +141,7 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = args.torch_deterministic
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+    print(f'device: {device}')
 
     # env setup
     env = make_env(args.map_name)
