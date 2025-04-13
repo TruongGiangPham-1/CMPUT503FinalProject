071a1367ec5befbb453a45a2a8d4e778d8bda747
TruongGiangPham-1 <truonggi@ualberta.ca>
2025-04-12 18:00:17 -0600

    fix: cc script added velocity and mapname flag

--- CC_script.sh
+++ CC_script.sh
@@ -20,4 +20,4 @@ source /home/truonggi/scratch/CMPUT503FinalProject/env/bin/activate
 
 echo $1  # run label
 
-python3 ppo.py --total_timesteps 1000000 --num_steps 2048  --run_label $1 --video
+python3 ppo.py --total_timesteps 1000000 --num_steps 2048  --run_label $1 --video  --car_velocity 0.7 --map_name straight_road
